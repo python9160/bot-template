d1f6e18143ba5790c2d3910a9afad4f33e4aaa21
Soumyadeep <rana.das10011@gmail.com>
2021-02-08 13:38:02 +0000

    i added the bot template

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bot-template
+
+This is a simple bot template for discord.js
---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+require('dotenv').config()
+
+const prefix = 'b!'
+
+client.on('ready', () => {
+  console.log(`Logged on as ${client.user.tag}`)
+});
+
+client.on('message', (msg) => {
+  if (msg.content.toLowerCase().startsWith(prefix)) {
+    const mess = msg.content.toLowerCase().substring(prefix.length);
+  }
+  if (mess === 'hi') {
+    msg.channel.send('Hi!')
+  }
+});
+
+if (process.env.BOT_TOKEN === 'bot token here') {
+  console.log('Change the Bot token to run the bot')
+} else {
+  client.login(process.env.BOT_TOKEN)
+    .catch((e) => {
+      console.log("Invalid Token\n"+e)
+    })
+}
